72fa211d563646784a9beb5d4c7cbafa3bc63eac
angelator312 <angel.raev2012@gmail.com>
2022-11-04 14:18:09 +0200

    index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Filestore } = require('./jsonwjs')
 const chasove = new Filestore('chasove.json')
 const app = express();
 const templatePath = path.join(__dirname, 'views');
-const port = 8080;
+const port = process.env.PORT ?? 8080;
 function breakdate(data) {
     return {
         year: data.getFullYear(),
