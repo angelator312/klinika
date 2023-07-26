33fba73973adcf12d03a5fd092a36f9354d7c19b
angelator312 <angel.raev2012@gmail.com>
2023-07-26 16:24:06 +0300

    create chasove json is not exist

--- jsonwjs.js
+++ jsonwjs.js
@@ -1,30 +1,38 @@
-const fs = require('fs/promises');
+const fs = require("fs/promises");
+const fss = require("fs");
 class Filestore {
-    constructor(file) {
-        this.file = file
+  constructor(file) {
+    this.file = file;
+    try {
+      fss.statSync(this.file);
+    } catch (err) {
+      if (err.code == "ENOENT") {
+        this.write({});
+      }
+      throw err;
     }
-    write(obj) {
-        return fs.writeFile(this.file, JSON.stringify(obj, null, 2))
-    }
-    async read() {
-        return JSON.parse(await fs.readFile(this.file))
-    }
-    async addkey(key, value) {
-        const data = await this.read()
-        data[key] = value
-        return this.write(data)
-    }
-    async deletekey(key) {
-        const data = await this.read()
-        delete data[key]
-        return this.write(data)
-
-    }
-    async getkey(key) {
-        const data = await this.read()
-        return data[key]
-    }
+  }
+  write(obj) {
+    return fs.writeFile(this.file, JSON.stringify(obj, null, 2));
+  }
+  async read() {
+    return JSON.parse(await fs.readFile(this.file));
+  }
+  async addkey(key, value) {
+    const data = await this.read();
+    data[key] = value;
+    return this.write(data);
+  }
+  async deletekey(key) {
+    const data = await this.read();
+    delete data[key];
+    return this.write(data);
+  }
+  async getkey(key) {
+    const data = await this.read();
+    return data[key];
+  }
 }
 module.exports = {
-    Filestore
-}
+  Filestore,
+};
